7f3b2a0bff8a7020b84e693772ea6f53520f335a
Alina <pisarenko.alina@gmail.com>
2022-04-06 15:48:36 -0400

    commit tree 88862aee18cb04840dae3f6d9e2dc3e8349c120b

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution here
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+function findMatching(list, someName) {
+  let matchingName = list.filter(
+    (name) => name.toLowerCase() === someName.toLowerCase()
+  );
+  return matchingName;
+}
+findMatching(drivers, "bobby");
+
+function fuzzyMatch(list, someLetters) {
+  let matchingLetters = list.filter((name) => name[0] === someLetters[0]);
+  return matchingLetters;
+}
+
+const driversTwo = [
+  {
+    name: "Bobby",
+    hometown: "Pittsburgh",
+  },
+  {
+    name: "Sammy",
+    hometown: "New York",
+  },
+  {
+    name: "Sally",
+    hometown: "Cleveland",
+  },
+  {
+    name: "Annette",
+    hometown: "Los Angeles",
+  },
+  {
+    name: "Bobby",
+    hometown: "Tampa Bay",
+  },
+];
+
+function matchName(list, someName) {
+  let matchingName = list.filter((item) => item.name === someName);
+
+  return matchingName;
+}
+matchName(driversTwo, "bobby");
